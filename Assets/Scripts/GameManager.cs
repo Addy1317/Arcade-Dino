a0f7fcb9106ad3771e149c2b4b3134165c8f6eef
Aditya Singh <s.addy1317@gmail.com>
2024-05-18 22:45:00 +0530

    feat: Hiscore - Hiscore Ui - Hiscore logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 namespace SlowpokeStudio.ArcadeDino
@@ -12,11 +13,19 @@ namespace SlowpokeStudio.ArcadeDino
 
         [SerializeField] private float _initalGameSpeed = 5f;
         [SerializeField] private float _gameSpeedIncreases = 0.1f;
-        [SerializeField] private GameObject _gameOverPanel;
+
+        [SerializeField] private TextMeshProUGUI _gameOverText;
+        [SerializeField] private Button _retryButton;
+
+        [SerializeField] private TextMeshProUGUI _scoreText;
+        [SerializeField] private TextMeshProUGUI _hiScoreText;
         internal float gameSpeed {get; private set; }
 
         private PlayerController _playerController;
         private Spawner _spawner;
+
+        private float _score;
+
 
         private void Awake()
         {
@@ -48,6 +57,8 @@ namespace SlowpokeStudio.ArcadeDino
         private void Update()
         {
             gameSpeed += _gameSpeedIncreases * Time.deltaTime;
+            _score += gameSpeed * Time.deltaTime;
+            _scoreText.text = Mathf.FloorToInt(_score).ToString("D5");
         }
 
         public void NewGame()
@@ -59,12 +70,16 @@ namespace SlowpokeStudio.ArcadeDino
                 Destroy(obstacle.gameObject);
             }
 
+            _score = 0f;
             gameSpeed = _initalGameSpeed;
+         
             enabled = true;
 
             _playerController.gameObject.SetActive(true);
             _spawner.gameObject.SetActive(true);
-            _gameOverPanel.SetActive(false);
+            _gameOverText.gameObject.SetActive(false);
+            _retryButton.gameObject.SetActive(false);
+            UpdateHiScore();
         }
 
         public void GameOver()
@@ -73,8 +88,24 @@ namespace SlowpokeStudio.ArcadeDino
             enabled = false;
             _playerController.gameObject.SetActive(false);
             _spawner.gameObject.SetActive(false);
-            _gameOverPanel.SetActive(true);
+            _gameOverText.gameObject.SetActive(true);
+            _retryButton.gameObject.SetActive(true);
+            UpdateHiScore();
         }
         
+        private void UpdateHiScore()
+        {
+            float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+             
+            if(_score > hiscore)
+            {
+                hiscore = _score;
+                PlayerPrefs.SetFloat("hiscore", hiscore);
+            }
+
+            _hiScoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
+
+        }
+
     }
 }
